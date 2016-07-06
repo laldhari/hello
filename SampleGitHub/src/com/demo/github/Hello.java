31943e420a1831b59f38e4d4dad16bdb545c600f
sdayal <sdayal@manh.com>
2016-07-06 13:33:11 +0530

    2

--- SampleGitHub/src/com/demo/github/Hello.java
+++ SampleGitHub/src/com/demo/github/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String args[]){
 		System.out.println("Hello Java");
 		System.out.println("first changes");
-		System.out.println("Shankar1");
+		System.out.println("Shankar12");
 		System.out.println("lal");
 		}
 }
